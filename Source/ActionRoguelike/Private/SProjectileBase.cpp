36ba878ba2825d894f3038a7dc8255dd928f17ef
Halil <77546808+yilmazhalilibr@users.noreply.github.com>
2024-02-17 17:29:26 +0300

    Wrote projetileBase class

---
+++ Source/ActionRoguelike/Private/SProjectileBase.cpp
@@ -0,0 +1,44 @@
+
+
+#include "SProjectileBase.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ASProjectileBase::ASProjectileBase()
+{
+	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
+	SphereComp->SetCollisionProfileName("Projectile");
+	SphereComp->OnComponentHit.AddDynamic(this, &ASProjectileBase::OnActorHit);
+	RootComponent = SphereComp;
+
+	EffectComp = CreateDefaultSubobject<UParticleSystemComponent>("EffectComp");
+	EffectComp->SetupAttachment(RootComponent);
+
+	MoveComp = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMoveComp");
+	MoveComp->bRotationFollowsVelocity = true;
+	MoveComp->bInitialVelocityInLocalSpace = true;
+	MoveComp->ProjectileGravityScale = 0.0f;
+	MoveComp->InitialSpeed = 8000;
+}
+void ASProjectileBase::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Explode();
+}
+
+void ASProjectileBase::Explode_Implementation()
+{
+	if (ensure(!IsPendingKill()))
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(this, ImpactVFX, GetActorLocation(), GetActorRotation());
+		Destroy();
+	}
+}
+
+void ASProjectileBase::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
---
+++ Source/ActionRoguelike/Public/ASDashProjectile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SProjectileBase.h"
+#include "ASDashProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API AASDashProjectile : public ASProjectileBase
+{
+	GENERATED_BODY()
+	
+};
